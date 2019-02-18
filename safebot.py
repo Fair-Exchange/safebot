9eb9eb083dc203aa75ffaa8d67a3f6be2f04be5f
3DA4C300 <3DA4C300@protonmail.com>
2019-02-18 15:31:37 +0100

    Remove pcmining pool

--- safebot.py
+++ safebot.py
@@ -67,10 +67,6 @@ class Bot(discord.Client):
             "API": "https://safe.bitpool.ro/api/stats",
             "fn": lambda content: json.loads(content)["pools"]["safecoin"]["hashrate"]*2/10**6,
         },
-        "http://safe.pcmining.xyz:8080/": {
-            "API": "http://safe.pcmining.xyz:8080/api/stats",
-            "fn": lambda content: json.loads(content)["pools"]["safecoin"]["hashrate"]*2/10**6,
-        },
         "https://safecoin.axepool.com/": {
             "API": "https://safecoin.axepool.com/api/stats",
             "fn": lambda content: json.loads(content)["pools"]["safecoin"]["hashrate"]*2/10**6,
